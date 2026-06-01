15b640e9625b855440fa1dd72a04eb800d3d719b
Arvin Yustin <arvin.yustin@amartha.com>
2026-06-01 23:03:36 +0700

    feat: add parent simulation instance

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import "github.com/arvinyustin99/traffic-simulation-v2/model"
 
+func main() {
+	model.InitTerminal()
+	newSimulation := model.NewSimulation()
+	newSimulation.Run()
 }
--- model/simulation.go
+++ model/simulation.go
@@ -1,22 +1,81 @@
 package model
 
 import (
+	"fmt"
 	"math/rand/v2"
 	"time"
+
+	"github.com/shopspring/decimal"
 )
 
+func NewSimulation() *Simulation {
+	lane_WE := &Lane{
+		ID:         0,
+		Cars:       make([]*Car, 0),
+		AllowSpawn: true,
+		SpawnRate:  decimal.NewFromFloat(0.1),
+		MaxSpeed:   2,
+		Direction:  Eastbound,
+	}
+	lane_EW := &Lane{
+		ID:         1,
+		Cars:       make([]*Car, 0),
+		AllowSpawn: true,
+		SpawnRate:  decimal.NewFromFloat(0.1),
+		MaxSpeed:   1,
+		Direction:  Westbound,
+	}
+	intersection_01 := NewIntersection()
+	intersection_01.RegisterLane(lane_WE, true)
+	intersection_01.RegisterLane(lane_EW, false)
+
+	return &Simulation{
+		TickRate: 2000 * time.Millisecond,
+		Running:  false,
+		Cars:     make([]*Car, 0),
+		Lanes: []*Lane{
+			lane_WE, lane_EW,
+		},
+		Intersections: []*Intersection{
+			intersection_01,
+		},
+		MaxCars:      2,
+		SafeDistance: 2,
+		EventBus:     NewEventBus(),
+		Metrics:      NewMetrics(),
+		Renderer:     NewTerminalRenderer(),
+	}
+
+}
 func (s *Simulation) Run() {
-	for {
-		// s.Tick()
-		s.SpawnCars()
-		s.UpdateTrafficLights()
-		// s.MoveCars()
-		// s.ResolveCollision()
-		// s.Render()
-		time.Sleep(100 * time.Millisecond)
+	s.Running = true
+
+	ticker := time.NewTicker(s.TickRate)
+	defer ticker.Stop()
+
+	for s.Running {
+		<-ticker.C
+
+		s.Tick++
+		s.Step()
 	}
 }
+func (s *Simulation) Step() {
+	s.SpawnCars()
+	s.UpdateTrafficLights()
+	s.ComputeCarIntentions()
+	s.ApplySpeedAdjustments()
+	s.MoveCars()
+	s.ResolveIntersectionCrossing()
+	// s.DetectCongestion()
+	// s.CollectMetrics()
+	s.EmitEvents()
+	s.Render()
+}
 func (s *Simulation) SpawnCars() {
+	if len(s.Cars) == s.MaxCars {
+		return
+	}
 	for _, lane := range s.Lanes {
 
 		if !lane.AllowSpawn {
@@ -25,19 +84,22 @@ func (s *Simulation) SpawnCars() {
 
 		random := rand.Float64()
 		if random < lane.SpawnRate.InexactFloat64() {
-			s.Cars = append(s.Cars, &Car{
+			newCar := Car{
 				ID:           len(s.Cars) + 1,
 				Position:     0,
 				Speed:        0,
 				DesiredSpeed: lane.MaxSpeed,
 				Lane:         lane.ID,
-			})
+			}
+			s.Cars = append(s.Cars, &newCar)
+			lane.Cars = append(lane.Cars, &newCar)
 		}
 	}
 }
 
-func (s *Simulation) UpdateTrafficLights() {
-	for _, intersection := range s.Intersection {
-		intersection.TrafficLight.Update()
+func (s *Simulation) EmitEvents() {
+	events := s.EventBus.Flush()
+	for _, e := range events {
+		fmt.Println(e)
 	}
 }
